c342f9f4b02ed8c4ebf2df065a30d03cf0f7ff87
AgusMattiussi <ahmattiussi@gmail.com>
2026-01-28 12:28:24 -0300

    Set up environment for agentic rag

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def main():
-    print("Hello ReAct LangGraph with Function Calling")
+    print("Hello agentic rag wit langgraph")
 
 
 if __name__ == "__main__":
